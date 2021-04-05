0059879ac8547d503ebb16ed5c992c58a23d4a20
Avi Anand <anand5895@outlook.com>
2021-04-06 00:38:52 +0530

    yhb

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import reducers from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const reduxSagaMonitorOptions = {};
+const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+
+const CLIENT_ID = process.env.REACT_APP_AUTH_CLIENT_ID;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/utils/urls.js
@@ -0,0 +1,8 @@
+export const ADMIN_URLS = {
+  CUSTOMERS: "/admin/customer",
+  ORDERS: "/admin/order",
+  PRODUCTS: "/admin/product",
+  STAFFS: "/admin/staff",
+};
+
+export const baseURL = "https://test.silverspoonbakers.in";
